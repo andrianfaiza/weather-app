dff82b209e8a291feca892195ac3e64d9be6e208
AndrianFaiza <faizaandrian@gmail.com>
2026-05-13 13:24:25 +0700

    Revert "fixed javascript"
    This reverts commit e9d72a1a66c7189ac2b480f590b7bf8f35919fc6.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-const apiKey = "API_KEY_IN_HERE"; //Replace to your api key
+const apiKey = "7dfca5cd72c557441775d613bbb83126";
 
 function formatLocalTime(utcSeconds, timezoneOffsetSeconds) {
   const localTimestamp = (utcSeconds + timezoneOffsetSeconds) * 1000;
